50d4304d8c3e03831d66f885131b823229bac07f
Shadekul Shovo <shadekuljamalpur.bd21@gmail.com>
2022-03-26 21:38:06 +0600

    trying to fix choose random2

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -3,19 +3,33 @@ import React from "react";
 const Cart = (props) => {
   const { cart } = props;
   console.log(cart);
+
+  function chooseME() {
+    return Math.floor(Math.random() * cart.length);
+  }
+
+  const random = chooseME();
+
   return (
     <div className="cart">
       <h1>Select Your Bat</h1>
       <p>Select Item Length:{cart.length}</p>
+
       <div>
         {cart.map((item) => (
           <h4>
             <li>{item.name}</li>
           </h4>
         ))}
+
         <div>
-          <button className="btn btn-primary">Choose For Me</button>
+          <button onClick="chooseMe" className="btn btn-primary">
+            Choose For Me
+          </button>
           <button className="btn btn-danger"> Clear</button>
+        </div>
+        <div>
+          <h2>{random}</h2>
         </div>
       </div>
     </div>
